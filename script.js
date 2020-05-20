7b7224e17fdcbf878bcc4f39fb0316b17deeac75
AymanElmanas <ayye942@gmail.com>
2020-05-20 13:36:26 +0300

    step1.2

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 $(document).ready(function(){
+    var 
+  
+
+    txt_1 = document.getElementById('txt_1');
     
-});
+    
+    var data = {
+        PersonalDetails: {
+            JobTitle: txt_1,
+            FirstName: FirstName,
+            LastName: LastName,
+            Email: Email,
+            Phone: Phone,
+        },
+        PersonalDetails: {
+            JobTitle: JobTitle,
+            FirstName: FirstName,
+            LastName: LastName,
+            Email: Email,
+            Phone: Phone
+        }
+
+    }
+    });
+
 
